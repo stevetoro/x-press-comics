b737a3093b4f4b920b1c92edf4559d10cfc469b8
Steve Toro <steve@stevetoro.com>
2018-07-15 01:13:37 -0400

    Create Artist table in SQLite database.

---
+++ migrations.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.sqlite');
+
+db.serialize(() => {
+  db.run("DROP TABLE IF EXISTS Artist");
+  db.run(`
+    CREATE TABLE Artist (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      date_of_birth TEXT NOT NULL,
+      biography TEXT NOT NULL,
+      is_currently_employed INTEGER DEFAULT 1
+    )`);
+});
