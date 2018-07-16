8b27f2a69e5a91578123f6c11fe6e9f153e81ede
Steve Toro <steve@stevetoro.com>
2018-07-16 00:06:05 -0400

    Create Series table.

--- migrations.js
+++ migrations.js
@@ -11,4 +11,11 @@ db.serialize(() => {
       biography TEXT NOT NULL,
       is_currently_employed INTEGER DEFAULT 1
     )`);
+  db.run("DROP TABLE IF EXISTS Series");
+  db.run(`
+    CREATE TABLE Series (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      description TEXT NOT NULL
+    )`);
 });
