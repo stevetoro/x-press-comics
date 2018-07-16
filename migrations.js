27452bcbce9f3759096516aef12bad7d92662acb
Steve Toro <steve@stevetoro.com>
2018-07-16 02:34:51 -0400

    Create Issue table.

--- migrations.js
+++ migrations.js
@@ -11,11 +11,25 @@ db.serialize(() => {
       biography TEXT NOT NULL,
       is_currently_employed INTEGER DEFAULT 1
     )`);
+    
   db.run("DROP TABLE IF EXISTS Series");
   db.run(`
     CREATE TABLE Series (
       id INTEGER PRIMARY KEY,
       name TEXT NOT NULL,
       description TEXT NOT NULL
+    )`);
+
+  db.run("DROP TABLE IF EXISTS Issue");
+  db.run(`
+    CREATE TABLE Issue (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      issue_number INTEGER NOT NULL,
+      publication_date TEXT NOT NULL,
+      artist_id INTEGER NOT NULL,
+      series_id INTEGER NOT NULL,
+      FOREIGN KEY (artist_id) REFERENCES Artist(id),
+      FOREIGN KEY (series_id) REFERENCES Series(id)
     )`);
 });
