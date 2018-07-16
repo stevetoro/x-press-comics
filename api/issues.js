3b7573c74a319fa7668cb436d32f954a9ad642a5
Steve Toro <steve@stevetoro.com>
2018-07-16 03:59:12 -0400

    Post new issue route.

--- api/issues.js
+++ api/issues.js
@@ -10,4 +10,32 @@ issuesRouter.get('/', (req, res, next) => {
   });
 });
 
+issuesRouter.post('/', (req, res, next) => {
+  const { name, issue_number, publication_date, artist_id } = req.body.issue;
+  if (!(name && issue_number && publication_date && artist_id)) return res.sendStatus(400);
+
+  db.get(`SELECT * FROM Artist WHERE Artist.id = ${artist_id}`, (err, artist) => {
+    if (err) return next(err);
+    if (!artist) return res.sendStatus(400);
+
+    db.run(
+      `INSERT INTO Issue (name, issue_number, publication_date, artist_id, series_id)
+      VALUES ($name, $issue_number, $publication_date, $artist_id, $series_id)`,
+      {
+        $name: name,
+        $issue_number: issue_number,
+        $publication_date: publication_date,
+        $artist_id: artist_id,
+        $series_id: req.params.seriesId
+      },
+      function (err) {
+        if (err) return next(err);
+        db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`, (err, issue) => {
+          if (err) return next(err);
+          return res.status(201).send({ issue });
+        });
+      })
+  });
+});
+
 module.exports = issuesRouter;
